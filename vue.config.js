5aaeceb5144a7ba00b884ea30b379cd6e6778356
Leandro Cunha <leandroscunha@gmail.com>
2022-11-18 22:00:59 +0100

    feat: add image-webpack-loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule("image-webpack")
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use("file-loader")
+      .loader("image-webpack-loader")
+      .options({
+        bypassOnDebug: true,
+        disable: true,
+      });
+  },
 });
