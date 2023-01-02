57e7059c9059b0e3e48044561ba5a66f1f4a33db
Leandro Cunha <leandroscunha@gmail.com>
2023-01-02 10:16:46 +0100

    feat: add Pina and list movies throught state management

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -4,33 +4,35 @@
     devtools)
   </p>
   <!-- a tall section title to test the lazy load images -->
-  <MoviePoster />
-  <ImageItem
-    source="https://media-cache.cinematerial.com/p/500x/ffziyoju/kill-bill-vol-2-movie-poster.jpg?v=1456295956"
-  />
-
-  <ImageItem
-    source="https://media-cache.cinematerial.com/p/500x/ouo9ahgl/four-rooms-movie-poster.jpg?v=1456742758"
-  />
-
-  <ImageItem
-    source="https://media-cache.cinematerial.com/p/500x/pkyii968/jackie-brown-movie-poster.jpg?v=1630441893"
-  />
+  <span v-for="movie in movies" v-bind:key="movie.id">
+    <div class="movie">
+      <ImageItem :source="movie.poster_path" />
+      <MovieDetails v-bind="movieById(movie.id)" />
+    </div>
+  </span>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import MoviePoster from "./MoviePoster.vue";
 import ImageItem from "./ImageItem.vue";
+import MovieDetails from "./MovieDetails.vue";
+import { useMovieStore } from "@/stores/MovieStore";
 
 export default defineComponent({
+  setup() {
+    const store = useMovieStore();
+    return { ...store };
+  },
   name: "MoviesList",
-  components: { MoviePoster, ImageItem },
+  components: { ImageItem, MovieDetails },
 });
 </script>
 
 <style>
 .tall-section-title {
   height: 100vh;
+}
+.movie {
+  display: flex;
 }
 </style>
